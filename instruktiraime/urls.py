b58ccf4827030cb7112bac800a48bedc8ba0692d
Georgi St. Lukanov <goshiii87@gmail.com>
2025-06-12 21:54:45 +0300

    .

--- instruktiraime/urls.py
+++ instruktiraime/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from .instruktiraime_web.views import chatbot_view
+
 urlpatterns = (
     path('admin/', admin.site.urls),
     path('', include('instruktiraime.instruktiraime_web.urls')),
+    path("chatbot/", chatbot_view, name="chatbot"),
 )
